acdb1b3dcbd8290f2e8d26b6630ec52f37e613c9
mwoussen <maurin.woussen@inrae.fr>
2022-04-29 15:10:41 +0200

    Ajoute un setup.py pour installer sous forme de package

---
+++ setup.py
@@ -0,0 +1,13 @@
+import sys
+from setuptools import setup, find_packages
+
+if sys.version_info < (3, 0):
+    print('ERROR: requires at least Python 3.0 to run.')
+    sys.exit(1)
+
+setup(
+    name="LightVegeManager for Singularity",
+    version="0.0.0 (Working only with Wheat-Fspm)",
+    url="https://github.com/mwoussen/lightvegemanager/tree/singularity",
+    packages=find_packages(include=["PyRATP/pyratp","src","runscript"], exclude=["PyRATP"]),
+)
