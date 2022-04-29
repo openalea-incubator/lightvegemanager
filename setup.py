db4c23f2a4b0be962413d49b1af14e0d125dca11
mwoussen <maurin.woussen@inrae.fr>
2022-04-29 17:24:31 +0200

    ajout de la recette du container

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info < (3, 0):
 
 setup(
     name="LightVegeManager for Singularity",
-    version="0.0.0 (Working only with Wheat-Fspm)",
+    version="0.0.0 (Works only with Wheat-Fspm)",
     url="https://github.com/mwoussen/lightvegemanager/tree/singularity",
     packages=find_packages(include=["PyRATP","src","runscript"]),
     package_data = {'' : ['*.pyd', '*.so'],}
