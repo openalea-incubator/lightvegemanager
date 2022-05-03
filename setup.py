5ca7c4734bde513d53626f466cd53f879d445f1e
mwoussen <maurin.woussen@inrae.fr>
2022-05-03 17:42:44 +0200

    import les dossiers?

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 import sys
 from setuptools import setup, find_packages
 
+import src, runscripts, PyRATP
+
 if sys.version_info < (3, 0):
     print('ERROR: requires at least Python 3.0 to run.')
     sys.exit(1)
 
 setup(
-    name="LightVegeManager for Singularity",
-    version="0.0.0 (Works only with Wheat-Fspm)",
+    name="LightVegeManager-Singularity",
+    version="0.0.0",
+    description="Works only with cn-wheat",
     url="https://github.com/mwoussen/lightvegemanager/tree/singularity",
-    packages=find_packages(include=["PyRATP","src","runscript"]),
+    packages=find_packages(),
     package_data = {'' : ['*.pyd', '*.so'],}
 )
