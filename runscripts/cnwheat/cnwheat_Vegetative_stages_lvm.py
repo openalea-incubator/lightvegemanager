fbd536859e4ca3ba43d9ab38ca5877ef2c3bdf8f
mwoussen <maurin.woussen@inrae.fr>
2022-06-03 10:24:03 +0200

    corrections activation calcul du rayonnement

--- runscripts/cnwheat/cnwheat_Vegetative_stages_lvm.py
+++ runscripts/cnwheat/cnwheat_Vegetative_stages_lvm.py
@@ -214,9 +214,7 @@ def simulation(level_tesselation, SIMULATION_LENGTH, outfolderpath, active_light
     tot_light = 0.
     
     #for t_light in progressbar.progressbar(range(START_TIME, SIMULATION_LENGTH, LIGHT_TIMESTEP)):
-    if active_lightmodel == "ratp" : simu_step = 1
-    elif active_lightmodel == "caribu" : simu_step = 4
-    for t_light in progressbar.progressbar(range(START_TIME, SIMULATION_LENGTH, simu_step)):
+    for t_light in progressbar.progressbar(range(START_TIME, SIMULATION_LENGTH, SENESCWHEAT_TIMESTEP)):
         if writing=="append":
             axes_all_data_list = []
             organs_all_data_list = []  # organs which belong to axes: roots, phloem, grains
@@ -248,8 +246,10 @@ def simulation(level_tesselation, SIMULATION_LENGTH, outfolderpath, active_light
         PARi_next_hours = meteo.loc[range(t_light, t_light + LIGHT_TIMESTEP), ['PARi']].sum().values[0]
 
         # vérifie si l'itération suivante est encore le jour? et lance le calcul de lumière
-        # if (t_light % LIGHT_TIMESTEP == 0) and (PARi_next_hours > 0):
-        if (PARi > 0):
+        if active_lightmodel == "ratp" : light_run = (PARi > 0)
+        elif active_lightmodel == "caribu" : light_run = (t_light % LIGHT_TIMESTEP == 0) and (PARi_next_hours > 0)
+
+        if light_run:
             # création d'un couvert hétérogène
             scene_etendu, domain = create_heterogeneous_canopy_copy(adel_wheat, g, nplants=50, var_plant_position=0.03, var_leaf_inclination=0.157, var_leaf_azimut=1.57, var_stem_azimut=0.157,
                                         plant_density=PLANT_DENSITY[1], inter_row=0.15)
