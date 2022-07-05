3d581ab2d072251b3de294d5b0f7c95d7cd7e9d7
mwoussen <maurin.woussen@inrae.fr>
2022-07-05 15:11:34 +0200

    ajoute installation de WheatFspm dans mes instructions

--- myinstall_lightvegemanager.sh
+++ myinstall_lightvegemanager.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 # compile la partie fortran de PyRATP (pour linux 64bits)
+# Sur le méso@LR : à lancer dans un conda avec numpy!
 cd PyRATP/f90
 bash compile_pyratp_lin64.sh
+
+# installe Wheat-Fspm pour avoir les données d'entrée
+git clone --recurse-submodules https://github.com/openalea-incubator/WheatFspm.git
+cd WheatFspm
+git submodule update --remote
