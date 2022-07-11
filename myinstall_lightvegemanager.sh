ce8a0124a048f522098feb3f8b679793468cc5a1
mwoussen <maurin.woussen@inrae.fr>
2022-07-11 11:23:51 +0200

    modif installation

--- myinstall_lightvegemanager.sh
+++ myinstall_lightvegemanager.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-# convertit les retours chariots
-dos2unix PyRATP/f90/compile_pyratp_lin64.sh
-
 # rend exécutable les scripts du dossier runscripts
 chmod +x runscripts/cnwheat/*
 chmod +x runscripts/legume/*
-
-# compile la partie fortran de PyRATP (pour linux 64bits)
-# Sur le méso@LR : à lancer dans un conda avec numpy!
-cd PyRATP/f90
-bash compile_pyratp_lin64.sh
-cd ../..
 
 # installe Wheat-Fspm pour avoir les données d'entrée
 git clone --recurse-submodules https://github.com/openalea-incubator/WheatFspm.git
@@ -20,3 +11,9 @@ git submodule update --remote
 
 # conversion retour chariot windows to unix
 find . -type f -print0 | xargs -0 dos2unix
+
+# compile la partie fortran de PyRATP (pour linux 64bits)
+# Sur le méso@LR : à lancer dans un conda avec numpy!
+cd PyRATP/f90
+bash compile_pyratp_lin64.sh
+cd ../..
