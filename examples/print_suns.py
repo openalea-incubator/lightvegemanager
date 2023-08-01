5de7f252fcb537857fe80ba523e4768c21b26745
mwoussen <maurin.woussen@inrae.fr>
2023-08-01 13:22:16 +0200

    deplacement des examples du package et suppression des simulations pour déplacer vers autre package

--- examples/print_suns.py
+++ examples/print_suns.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     # definition
     # print_sun(day, hour, [longitude, latitude, timezone], truesolartime)
 
-    print_sun(201, 12, [0., 0., 0.] , True)
-    print_sun(201, 12, [40., 0., 0.] , True)
-    print_sun(201, 16, [40., 12., 2.] , True)
-    print_sun(201, 16, [40., 12., 2.] , False)
-    print_sun(347, 16, [46., 0., 0.] , True)
+    print_sun(201, 12, [0.0, 0.0, 0.0], True)
+    print_sun(201, 12, [40.0, 0.0, 0.0], True)
+    print_sun(201, 16, [40.0, 12.0, 2.0], True)
+    print_sun(201, 16, [40.0, 12.0, 2.0], False)
+    print_sun(347, 16, [46.0, 0.0, 0.0], True)
